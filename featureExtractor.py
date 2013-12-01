bde10f1aab2a23708125440df61f455fcd1d1cb8
Clay Schubiner <clayschubiner@Clays-MacBook-Pro.local>
2013-11-30 16:54:19 -0800

    more stuff

--- featureExtractor.py
+++ featureExtractor.py
@@ -1,24 +1,29 @@
 #! /usr/bin/python
 from firebase import firebase
+import json
+import topicWordMap
 
 firebase = firebase.FirebaseApplication('https://zeronews.firebaseio.com', None)
-result = firebase.get('/articles', None)
 
-politicsWords = set(['afghanistan', 'africa', 'america', 'american', 'army', 'asia', 'barack', 'bush', 'campaign', 'candidate', 'china', 'clinton', 'congress', 'conservative', 'constitution', 'corruption', 'courts', 'crime', 'culture', 'democrat', 'democrats', 'economics', 'economy', 'election', 'elections', 'energy', 'environment', 'europe', 'featured', 'funny', 'george', 'government', 'governor', 'health care', 'house', 'humor', 'immigration', 'india', 'iran', 'iraq', 'islam', 'israel', 'john', 'kerry', 'law', 'liberal', 'libya', 'mccain', 'media', 'military', 'new', 'obama', 'pakistan', 'palin', 'political', 'politics', 'poll', 'polling', 'polls', 'president', 'religion', 'rep', 'republican', 'republicans', 'russia', 'science', 'security', 'senate', 'senator', 'states', 'taxes', 'technology', 'terrorism', 'united', 'us', 'usa', 'war', 'washington', 'world'])
-sportsWords = set(['49ers', 'afl', 'american', 'football', 'athletics', 'auto', 'racing', 'badminton', 'baseball', 'basketball', 'basketball', 'm', 'basketball', 'w', 'bowling', 'boxing', 'boys', 'basketball', 'boys', 'soccer', 'cheerleading', 'crew', 'cricket', 'cross', 'country', 'curling', 'cycling', 'darts', 'derek', 'jeter', 'equestrian', 'fencing', 'field', 'hockey', 'fishing', 'football', 'basketball', 'girls', 'soccer', 'golf', 'gymnastics', 'handball', 'hockey', 'horse', 'racing', 'ice', 'hockey', 'indoor', 'track', 'lacrosse', 'lakers', 'league', 'martial', 'arts', 'men', 'mens', 'mens', 'basketball', 'mens', 'golf', 'mens', 'soccer', 'mens', 'sports', 'mens', 'tennis', 'michael', 'jordan', 'miscellaneous', 'mma', 'motor', 'sports', 'motorsport', 'motorsports', 'nascar', 'nba', 'netball', 'nfl', 'olympics', 'other', 'sports', 'poker', 'racing', 'sox', 'rifle', 'rowing', 'rugby', 'rugby', 'union', 'running', 'sailing', 'sf', 'skateboarding', 'skiing', 'snooker', 'soccer', 'softball', 'squash', 'surfing', 'swimming', 'swimming', 'diving', 'table', 'tennis', 'tennis', 'track', 'track', 'field', 'triathlon', 'volleyball', 'water', 'polo', 'winter', 'sports', 'women', 'womens', 'womens', 'basketball', 'womens', 'golf', 'womens', 'soccer', 'womens', 'sports', 'womens', 'tennis', 'wrestling', 'yankees'])
+articleList = firebase.get('/articles', None)
 
-politicsArticles = 0
-for article in result:
-  articleText = article['text'].encode('utf-8')
-  numPoliticsWords = 0
-  numSportsWords = 0
-  for word in articleText.split():
-    if word in politicsWords:
-      numPoliticsWords += 1
-    if word in sportsWords:
-      numSportsWords += 1
+topics = ['politics', 'sports', 'world', 'science', 'health', 'technology', 'business', 'gaming']
+wordMap = topicWordMap.getwordmap()
+for articleIndex, article in enumerate(articleList[:2]):
+  articleWordList = article['text'].encode('utf-8').split()
+  features = list()
+  for topic in topics:
+    wordCount = 0
+    topicWords = wordMap[topic]
+    for word in articleWordList:
+      if len(word) >= 4 and word.lower() in topicWords:
+        wordCount += 1
+    for i in range(len(articleWordList)-1):
+      # Find pairs of words
+      wordPair = articleWordList[i] + ' ' + articleWordList[i+1]
+      if wordPair.lower() in topicWords:
+        wordCount += 2
 
-  if float(numPoliticsWords)/article['wordCount'] > .01:
-    politicsArticles += 1
-  print numPoliticsWords, numSportsWords
-print politicsArticles
+    features.append(float(wordCount) * 1000.0/float(article['wordCount']) / float(len(topicWords)))
+  result = firebase.put('articles', str(articleIndex) + '/features', features)
+  print features
--- fireClay.py
+++ fireClay.py
@@ -44,46 +44,57 @@ timeout = 120
 socket.setdefaulttimeout(timeout)
 
 feed_url = "http://feeds.washingtonpost.com/rss/politics"
-feeds = [("http://feeds.washingtonpost.com/rss/politics", ['politics']),
-  ("http://feeds.washingtonpost.com/rss/lifestyle/food", ['lifestyle', 'food']),
-  ("http://feeds.washingtonpost.com/rss/entertainment/music", ['entertainment', 'music']),
-  ("http://feeds.washingtonpost.com/rss/entertainment/tv", ['entertainment', 'tv']),
-  ("http://feeds.washingtonpost.com/rss/entertainment/celebrities", ['entertainment', 'celebrities']),
-  ("http://feeds.washingtonpost.com/rss/entertainment/books", ['entertainment', 'books']),
-  ("http://feeds.washingtonpost.com/rss/lifestyle/travel", ['lifestyle', 'travel']),
-  ("http://feeds.washingtonpost.com/rss/lifestyle/style", ['lifestyle', 'style']),
-  ("http://feeds.washingtonpost.com/rss/lifestyle/advice", ['lifestyle', 'advice']),
-  ("http://feeds.washingtonpost.com/rss/business", ['business']),
-  ("http://feeds.washingtonpost.com/rss/world", ['world']),
-  ("http://feeds.washingtonpost.com/rss/sports", ['sports']),
-  ("http://www.npr.org/rss/rss.php?id=1014", []),
-  ("http://www.npr.org/rss/rss.php?id=1003", []),
-  ("http://feeds.washingtonpost.com/rss/homepage", []),
-  ("http://www.nytimes.com/services/xml/rss/nyt/National.xml?pagewanted=all", []),
-  ("http://www.npr.org/rss/rss.php?id=1009", []),
-  ("http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml?edition=int", []),
-  ("http://www.nytimes.com/services/xml/rss/nyt/International.xml?pagewanted=all", []),
-  ("http://www.npr.org/rss/rss.php?id=3", []),
-  ("http://feeds.bbci.co.uk/news/business/rss.xml?edition=int", []),
-  ("http://feeds.wired.com/wired/index", []),
-  ("http://www.pcworld.com/index.rss", []),
-  ("http://www.sciencemag.org/rss/current.xml", []),
-  ("http://www.npr.org/rss/rss.php?id=1025", []),
-  ("http://feeds.bbci.co.uk/news/health/rss.xml?edition=int", []),
-  ("http://www.washingtonpost.com/wp-dyn/rss/sports/index.xml", []),
-  ("http://feeds.theguardian.com/theguardian/football/rss", []),
-  ("http://www.npr.org/rss/rss.php?id=1055", []),
-  ("http://i.rottentomatoes.com/syndication/rss/top_movies.xml", []),
-  ("http://i.rottentomatoes.com/syndication/rss/in_theaters.xml", []),
-  ("http://www.nytimes.com/services/xml/rss/nyt/MovieNews.xml?pagewanted=all", []),
-  ("http://www.sciencemag.org/rss/twis.xml", []),
-  ("http://feeds.gawker.com/kotaku/vip", []),
-  ("http://feeds.gawker.com/gizmodo/vip", []),
-  ("http://feeds.gawker.com/lifehacker/vip", [])]
+feeds = ['http://feeds.washingtonpost.com/rss/politics',
+        'http://feeds.washingtonpost.com/rss/lifestyle/food',
+        'http://feeds.washingtonpost.com/rss/entertainment/music',
+        'http://feeds.washingtonpost.com/rss/entertainment/tv',
+        'http://feeds.washingtonpost.com/rss/entertainment/celebrities',
+        'http://feeds.washingtonpost.com/rss/entertainment/books',
+        'http://feeds.washingtonpost.com/rss/lifestyle/travel',
+        'http://feeds.washingtonpost.com/rss/lifestyle/style',
+        'http://feeds.washingtonpost.com/rss/lifestyle/advice',
+        'http://feeds.washingtonpost.com/rss/business',
+        'http://feeds.washingtonpost.com/rss/world',
+        'http://feeds.washingtonpost.com/rss/sports',
+        'http://www.npr.org/rss/rss.php?id=1014',
+        'http://www.npr.org/rss/rss.php?id=1003',
+        'http://feeds.washingtonpost.com/rss/homepage',
+        'http://www.nytimes.com/services/xml/rss/nyt/National.xml?pagewanted=all',
+        'http://www.npr.org/rss/rss.php?id=1009',
+        'http://feeds.bbci.co.uk/news/world/us_and_canada/rss.xml?edition=int',
+        'http://www.nytimes.com/services/xml/rss/nyt/International.xml?pagewanted=all',
+        'http://www.npr.org/rss/rss.php?id=3',
+        'http://feeds.bbci.co.uk/news/business/rss.xml?edition=int',
+        'http://feeds.wired.com/wired/index',
+        'http://www.pcworld.com/index.rss',
+        'http://www.sciencemag.org/rss/current.xml',
+        'http://www.npr.org/rss/rss.php?id=1025',
+        'http://feeds.bbci.co.uk/news/health/rss.xml?edition=int',
+        'http://www.washingtonpost.com/wp-dyn/rss/sports/index.xml',
+        'http://feeds.theguardian.com/theguardian/football/rss',
+        'http://www.npr.org/rss/rss.php?id=1055',
+        'http://i.rottentomatoes.com/syndication/rss/top_movies.xml',
+        'http://i.rottentomatoes.com/syndication/rss/in_theaters.xml',
+        'http://www.nytimes.com/services/xml/rss/nyt/MovieNews.xml?pagewanted=all',
+        'http://www.sciencemag.org/rss/twis.xml',
+        'http://feeds.gawker.com/kotaku/vip',
+        'http://feeds.gawker.com/gizmodo/vip',
+        'http://feeds.gawker.com/lifehacker/vip']
 
 articles = []
 count = 0
-for feed_url, topic in feeds:
+
+def addArticle(url):
+  try:
+    testURl = 'http://readability.com/api/content/v1/parser?url=' + urllib.quote(url) + '&token=41595283b49e7c2c6be11082bb1a0696501be7ff'
+    response = json.loads(urllib2.urlopen(testURl).read())
+    text = unescape(str(nltk.clean_html(response['content'])))
+
+    articles.append({'title':title, 'url':url, 'text':text, 'wordCount':response['word_count'], 'author':response['author'], 'domain':response['domain'], 'date':response['date_published']})
+  except:
+    pass
+
+for feed_url in feeds:
   count += 1
   print count, 'of', len(feeds)
   d = feedparser.parse(feed_url)
@@ -93,17 +104,7 @@ for feed_url, topic in feeds:
     if url in articleURLs:
       continue
     articleURLs.add(url)
-    try:
-      testURl = 'http://readability.com/api/content/v1/parser?url=' + urllib.quote(url) + '&token=41595283b49e7c2c6be11082bb1a0696501be7ff'
-      response = json.loads(urllib2.urlopen(testURl).read())
-      topics = list()
-      topics.extend(topic)
-      text = unescape(str(nltk.clean_html(response['content'])))
-
-      articles.append({'title':title, 'url':url, 'text':text, 'wordCount':response['word_count'], 'author':response['author'], 'domain':response['domain'], 'date':response['date_published'], 'topics':topics})
-    except:
-      pass
-
+    addArticle(url)
 
 fOutput = open('exportedArticles.json', 'w')
 fOutput.write(json.dumps({'articles':articles}))
